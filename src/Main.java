f03d97b55bd6ba3aba7749924fffa425a7dad8e0
PCAdmin <cursivet9@gmail.com>
2025-04-10 01:41:44 +0300

    легенда

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Создаём репозиторий и водителя
+
         DriverRepository repository = new DriverRepository();
         Driver driver = new Driver(1, "Иван Петров", true);
         repository.save(driver);
 
-        // Создаём транзакционный скрипт
         ScheduleTransactionScript transactionScript = new ScheduleTransactionScript(repository);
 
-        // Добавляем смены
         transactionScript.addWorkShiftToDriver(1, LocalDateTime.of(2024, 4, 1, 2, 0), LocalDateTime.of(2024, 4, 1, 4, 0));
         transactionScript.addWorkShiftToDriver(1, LocalDateTime.of(2024, 4, 1, 6, 0), LocalDateTime.of(2024, 4, 2, 18, 0));
 
-        // Проверяем список смен
         List<?> shifts = transactionScript.getShiftsForDriverOnDate(1, LocalDateTime.of(2024, 4, 1, 0, 0));
         System.out.println("Смены на 2024-04-01: " + shifts);
 
-        // Проверяем доступность водителя
         boolean available = transactionScript.checkDriverAvailability(1, LocalDateTime.of(2024, 4, 1, 12, 0));
         System.out.println("Доступность водителя в 12:00: " + available);
 
-        // Проверяем доступность водителя
         available = transactionScript.checkDriverAvailability(1, LocalDateTime.of(2024, 4, 1, 5, 0));
         System.out.println("Доступность водителя в 05:00: " + available);
     }
